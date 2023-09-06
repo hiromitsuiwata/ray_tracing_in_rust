520e5594111bd6d71cabd57e9d74fb0acb6bb3c4
Hiromitsu Iwata <hiromitsu7@gmail.com>
2023-09-06 23:25:15 +0900

    png

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
+use image;
+
 fn main() {
-    const IMAGE_WIDTH: u16 = 256;
-    const IMAGE_HEIGHT: u16 = 256;
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    let mut img = image::RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     let height = (IMAGE_HEIGHT - 1) as f64;
     let width = (IMAGE_WIDTH - 1) as f64;
 
-    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        let r = (x as f64) / width;
+        let g = ((IMAGE_HEIGHT - y) as f64) / height;
+        let b = 0.25;
 
-    for j in (0..IMAGE_HEIGHT).rev() {
-        for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / width;
-            let g = (j as f64) / height;
-            let b = 0.25;
-
-            let ir = (255.999 * r) as u16;
-            let ig = (255.999 * g) as u16;
-            let ib = (255.999 * b) as u16;
-
-            println!("{} {} {}", ir, ig, ib);
-        }
+        let ir = (255.999 * r) as u8;
+        let ig = (255.999 * g) as u8;
+        let ib = (255.999 * b) as u8;
+        *pixel = image::Rgb([ir, ig, ib]);
     }
+    img.save("result.png").unwrap();
 }
