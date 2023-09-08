758367089aba7b54dbf30f0f46c8d0d20fcb078a
Hiromitsu Iwata <hiromitsu7@gmail.com>
2023-09-09 01:39:42 +0900

    アンチエイリアシング

--- src/color.rs
+++ src/color.rs
@@ -2,9 +2,25 @@ use image::Rgb;
 
 use crate::vec3::Vec3;
 
-pub fn write_color(pixel: &mut Rgb<u8>, color: Vec3) {
-    let ir = (255.999 * color.r()) as u8;
-    let ig = (255.999 * color.g()) as u8;
-    let ib = (255.999 * color.b()) as u8;
+pub fn write_color(pixel: &mut Rgb<u8>, color: Vec3, sample_per_pixel: u32) {
+    let scale: f64 = 1.0 / sample_per_pixel as f64;
+
+    let r = color.r() * scale;
+    let g = color.g() * scale;
+    let b = color.b() * scale;
+
+    let ir = (256.0 * clamp(r, 0.0, 0.999)) as u8;
+    let ig = (256.0 * clamp(g, 0.0, 0.999)) as u8;
+    let ib = (256.0 * clamp(b, 0.0, 0.999)) as u8;
     *pixel = image::Rgb([ir, ig, ib]);
+}
+
+fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        min
+    } else if max < x {
+        max
+    } else {
+        x
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::f64::INFINITY;
+use std::io;
+use std::io::Write;
 
 use hitrecord::HitRecord;
 use hittable::Hittable;
@@ -14,10 +16,11 @@ mod vec3;
 use camera::Camera;
 use color::*;
 use item::Sphere;
+use rand::Rng;
 use ray::Ray;
 use vec3::{color, origin, unit_vector, Vec3};
 
-fn ray_color(ray: &Ray, world: Vec<Box<dyn Hittable>>) -> Vec3 {
+fn ray_color(ray: &Ray, world: &Vec<Box<dyn Hittable>>) -> Vec3 {
     let iter = world.iter();
 
     // 物体に衝突する場合
@@ -54,41 +57,61 @@ fn ray_color(ray: &Ray, world: Vec<Box<dyn Hittable>>) -> Vec3 {
 fn main() {
     // 定数設定
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u32 = 384;
+    const IMAGE_WIDTH: u32 = 225;
     const IMAGE_HEIGHT: u32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u32;
     const HEIGHT: f64 = (IMAGE_HEIGHT - 1) as f64;
     const WIDTH: f64 = (IMAGE_WIDTH - 1) as f64;
+    const SAMPLE_PER_PIXEL: u32 = 20;
+    const NUM_OF_PIXELS: u32 = IMAGE_WIDTH * IMAGE_HEIGHT;
 
     let camera = Camera::new(ASPECT_RATIO, IMAGE_WIDTH, 2.0, 1.0, origin());
 
     let mut img = image::RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
+    // 物体を配置
+    let mut world: Vec<Box<dyn Hittable>> = Vec::new();
+    world.push(Box::new(Sphere::new(Vec3::new(-0.2, 0.5, -1.6), 0.4)));
+    world.push(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
+    world.push(Box::new(Sphere::new(Vec3::new(0.2, 0.1, -0.6), 0.1)));
+    world.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.3)));
+
+    // 進捗
+    let mut progress: u32 = 0;
+
     // カメラから見える画角の1ピクセルごとに色を決めていく
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        // 画角の横座標
-        let u = (x as f64) / WIDTH;
-        // 画角の縦座標
-        // 画角の座標系では左上が(0, 0)なためy軸の向きが逆になっている
-        let v = (HEIGHT - (y as f64)) / HEIGHT;
-        let ray = camera.get_ray(u, v);
+        // アンチエイリアシングのために乱数を使って少しずらした方向にレイをたくさん飛ばして色を平均化する
+        // 平均を計算するために足しこむための変数
+        let mut sum_of_colors = origin();
+        // 乱数
+        let mut rng = rand::thread_rng();
+        for _ in 0..SAMPLE_PER_PIXEL {
+            // 乱数を生成
+            let rand1 = rng.gen::<f64>();
+            let rand2 = rng.gen::<f64>();
 
-        // 物体を配置
-        let mut world: Vec<Box<dyn Hittable>> = Vec::new();
-        world.push(Box::new(Sphere::new(Vec3::new(-0.2, 0.5, -1.6), 0.4)));
-        world.push(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
-        world.push(Box::new(Sphere::new(Vec3::new(0.2, 0.1, -0.6), 0.1)));
-        world.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.3)));
+            // 画角の横座標
+            let u = ((x as f64) + rand1) / (WIDTH - 1.0);
+            // 画角の縦座標
+            // 画角の座標系では左上が(0, 0)なためy軸の向きが逆になっている
+            let v = ((HEIGHT - 1.0) - ((y as f64) + rand2)) / (HEIGHT - 1.0);
+            let ray = camera.get_ray(u, v);
 
-        // レイを飛ばして色を決める
-        let color = ray_color(&ray, world);
+            // レイを飛ばして色を決める
+            let color = ray_color(&ray, &world);
+            // 足しこむ
+            sum_of_colors = sum_of_colors + color;
+        }
 
-        // ピクセルに色を塗る
-        write_color(pixel, color);
+        // ピクセルに色を塗る。サンプル数で割る
+        write_color(pixel, sum_of_colors, SAMPLE_PER_PIXEL);
 
         // 進捗を表示
-        if y == IMAGE_HEIGHT - 1 {
-            eprintln!("{:.1}%", 100.0 * (x as f64) / ((IMAGE_WIDTH - 1) as f64));
+        if progress % (NUM_OF_PIXELS / 20) == 0 {
+            println!("{:.0}%", 100.0 * (progress as f64) / NUM_OF_PIXELS as f64);
+            io::stdout().flush().unwrap();
         }
+        progress += 1;
     }
     img.save("result.png").unwrap();
 }
